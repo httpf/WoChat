fd45bdd10bbdbd08e39e8bcd5588456d53d77979
yao <yao.uki@qq.com>
2020-09-17 22:20:41 +0800

    server

--- server.cpp
+++ server.cpp
@@ -11,14 +11,34 @@
 #include <iostream>
 #include <thread>
 #include <list>
+#include <map>
+#include <string>
 
-#define PORT 7000
+#define PORT 7654
 #define IP "127.0.0.1"
 
 int s;
 struct sockaddr_in servaddr;
 socklen_t len;
-std::list<int> li; //用list来存放套接字，没有限制套接字的容量就可以实现一个server跟若干个client通信
+std::list<int> li;
+std::map<std::string, int> client;
+
+bool isNum(std::string str)
+{
+    for (int i = 0; i < str.size(); i++)
+    {
+        int tmp = (int)str[i];
+        if (tmp >= 48 && tmp <= 57)
+        {
+            continue;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 void getConn()
 {
@@ -30,10 +50,10 @@ void getConn()
     }
 }
 
-void getData()
+void getID()
 {
     struct timeval tv;
-    tv.tv_sec = 10; //设置倒计时时间
+    tv.tv_sec = 10;
     tv.tv_usec = 0;
     while (1)
     {
@@ -63,24 +83,92 @@ void getData()
                 char buf[1024];
                 memset(buf, 0, sizeof(buf));
                 int len = recv(*it, buf, sizeof(buf), 0);
-                printf("%s", buf);
+
+                int idIndex = strspn(buf, "myIDWas:");
+                if (idIndex > 12)
+                {
+                    char ID[1024] = {0};
+                    std::string sbuf(buf);
+                    std::string id = sbuf.substr(idIndex);
+
+                    if (isNum(id))
+                    {
+                        client[id] = *it;
+                        li.erase(it);
+                        std::cout << "New connected id: " << id << std::endl;
+                    }
+                }
             }
         }
         sleep(1);
     }
 }
 
-void sendMess()
+void getData()
 {
+    struct timeval tv;
+    tv.tv_sec = 10;
+    tv.tv_usec = 0;
     while (1)
     {
-        char buf[1024];
-        fgets(buf, sizeof(buf), stdin);
-        //printf("you are send %s", buf);
-        std::list<int>::iterator it;
-        for (it = li.begin(); it != li.end(); ++it)
+        std::map<std::string, int>::iterator it;
+        for (it = client.begin(); it != client.end(); ++it)
         {
-            send(*it, buf, sizeof(buf), 0);
+            fd_set rfds;
+            FD_ZERO(&rfds);
+            int maxfd = 0;
+            int retval = 0;
+            FD_SET(it->second, &rfds);
+            if (maxfd < it->second)
+            {
+                maxfd = it->second;
+            }
+            retval = select(maxfd + 1, &rfds, NULL, NULL, &tv);
+            if (retval == -1)
+            {
+                printf("select error\n");
+            }
+            else if (retval == 0)
+            {
+            }
+            else
+            {
+                char buf[1024];
+                memset(buf, 0, sizeof(buf));
+                int len = recv(it->second, buf, sizeof(buf), 0);
+                if(len > 0)
+                {
+                    printf("Message: %s\n", buf);
+                }
+                else if (len == 0)
+                {
+                    printf("%s closed!\n", it->first.c_str());
+                    client.erase(it);
+                }
+                else
+                {
+                    printf("%s socket error!\n", it->first.c_str());
+                    client.erase(it);
+                }
+
+            }
+        }
+        sleep(1);
+    }
+}
+
+void broadcastFromServer()
+{
+    char buf[1024];
+    while (1)
+    {
+        memset(buf, 0, 1024);
+        fgets(buf, sizeof(buf), stdin);
+
+        std::map<std::string, int>::iterator it;
+        for (it = client.begin(); it != client.end(); ++it)
+        {
+            send(it->second, buf, sizeof(buf), 0);
         }
     }
 }
@@ -105,21 +193,15 @@ int main()
     }
     len = sizeof(servaddr);
 
-    //thread : while ==>> accpet
     std::thread t(getConn);
-    t.detach(); //detach的话后面的线程不用等前面的进程完成后才能进行，如果这里改为join则前面的线程无法判断结束，就会
-    //一直等待，导致后面的线程无法进行就无法实现操作
-    //printf("done\n");
-    //thread : input ==>> send
-    std::thread t1(sendMess);
-    t1.detach();
-    //thread : recv ==>> show
-    std::thread t2(getData);
+    t.detach();
+
+    std::thread t2(getID);
     t2.detach();
-    while (1) //做一个死循环使得主线程不会提前退出
+    while (1)
     {
     }
+
+    close(s);
     return 0;
 }
-/*这个跟前面的不一样的地方是，把获得连接套接字getConn和发送信息sendMess和接收信息getData放在三个函数中，创建
-的三个线程分别对应处理三个函数，就可以使得server能跟若干个client通信*/
